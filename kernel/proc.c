8b7754a6dade97cbe05673ff0ee121131aec928f
yujt-moon <1291932524@qq.com>
2025-06-25 23:29:21 +0800

    提交 sysinfo 系统调用

--- kernel/proc.c
+++ kernel/proc.c
@@ -657,3 +657,17 @@ procdump(void)
     printf("\n");
   }
 }
+
+uint64
+procnum(void)
+{
+  uint64 procnum = 0;
+
+  struct proc *p;
+  for (p = proc; p < &proc[NPROC]; p++) {
+    if (p->state != UNUSED)
+      procnum++;
+  }
+
+  return procnum;
+}
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -6,6 +6,7 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
+#include "sysinfo.h"
 
 uint64
 sys_exit(void)
@@ -104,5 +105,23 @@ sys_trace(void)
     return -1;
 
   myproc()->mask = mask;
+  return 0;
+}
+
+uint64
+sys_sysinfo(void)
+{
+  struct proc *p = myproc();
+  uint64 sinfo;
+
+  if (argaddr(0, &sinfo) < 0)
+    return -1;
+
+  struct sysinfo sysinfo;
+
+  sysinfo.freemem = freemem();
+  sysinfo.nproc = procnum();
+  if (copyout(p->pagetable, sinfo, (char *)&sysinfo, sizeof(sysinfo)) < 0)
+    return -1;
   return 0;
 }
