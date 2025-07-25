925da61d5215dabc378dc79e3130fc26a1da00ef
yujt-moon <1291932524@qq.com>
2025-07-25 23:51:15 +0800

    vmprint实现

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,27 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+static void printwalk(pagetable_t pagetable, int level)
+{
+  if (level >= 3)
+    return;
+  // there are 2^9 = 512 PTEs in a page table
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if (pte & PTE_V) {
+      uint64 child = PTE2PA(pte);
+      for (int j = 0; j < level; j++) {
+        printf(".. ");
+      }
+      printf("..%d: pte %p pa %p\n", i, pte, child);
+      printwalk((pagetable_t)child, level + 1);
+    }
+  }
+}
+
+void vmprint(pagetable_t pagetable) 
+{
+  printf("page table %p\n", pagetable);
+  printwalk(pagetable, 0);
+}
