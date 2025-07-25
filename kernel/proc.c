cf0d8cfb139ec5706d68bd93b0d70b24cf8f806a
yujt-moon <1291932524@qq.com>
2025-07-25 22:45:45 +0800

    ugetpid实现

--- kernel/proc.c
+++ kernel/proc.c
@@ -127,6 +127,13 @@ found:
     return 0;
   }
 
+  // Allocate a sharing page.
+  if((p->usyscall = (struct usyscall *)kalloc()) == 0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -153,6 +160,8 @@ freeproc(struct proc *p)
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
+  if(p->usyscall)
+    kfree((void*)p->usyscall);
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
@@ -196,6 +205,16 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  // map the sharing area
+  if (mappages(pagetable, USYSCALL, PGSIZE,
+                (uint64)(p->usyscall), PTE_U | PTE_R) < 0) {
+    uvmunmap(pagetable, USYSCALL, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  } else {
+    p->usyscall->pid = p->pid;
+  }
+
   return pagetable;
 }
 
@@ -206,6 +225,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
